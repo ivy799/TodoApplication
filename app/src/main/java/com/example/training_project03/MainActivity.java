0f4af3bf64ee5f69eda6f31bd82de066bad0d775
ivy799 <muhammadraihanaan123@gmail.com>
2025-01-06 14:00:16 +0800

    finishing

--- app/src/main/java/com/example/training_project03/MainActivity.java
+++ app/src/main/java/com/example/training_project03/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.training_project03;
 
-import android.app.Dialog;
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AlertDialog;
-import android.content.DialogInterface;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -20,30 +16,71 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AlertDialog dialog;
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button add;
-        AlertDialog dialog;
-        LinearLayout layout;
-
-        add = findViewById(R.id.addButton);
+        Button add = findViewById(R.id.addButton);
         layout = findViewById(R.id.taskContainer);
 
+        buildDialog();
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Dialog.show();
+                dialog.show();
             }
         });
     }
+
+    private void buildDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog, null);
+
+        final EditText name = view.findViewById(R.id.nameEdit);
+
+        builder.setView(view)
+                .setTitle("Add Task")
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        addCard(name.getText().toString());
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        dialog = builder.create();
+    }
+
+    private void addCard(String name){
+        final View view = getLayoutInflater().inflate(R.layout.card,null);
+
+        TextView nameView = view.findViewById(R.id.name);
+        Button delete = view.findViewById(R.id.delete);
+        nameView.setText(name);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+        layout.addView(view);
+
+
+    }
+
+
 }
